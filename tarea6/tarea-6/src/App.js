fb29bfaae2da9bf3f77c3e96cf8004fb147f6d7e
Constanza Beneventi <constanza.beneventi@uniacc.edu>
2025-11-28 21:55:48 -0300

    Add files via upload

---
+++ tarea6/tarea-6/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Header from './Header';
+import Boton from './Boton';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>TAREA 6 YAY!!!</p>
+        <Header />
+        <Boton />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tarea6/tarea-6/src/Header.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Boton from './Boton';
+
+function Header() {
+    return (
+        <div className='header'>
+   
+        </div>
+    );
+}
+
+export default Header; 
